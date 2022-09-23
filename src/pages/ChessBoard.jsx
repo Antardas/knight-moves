728ea5bd547d7678278db0a01cc577079616187e
Antar Das <antardas2334@gmail.com>
2022-09-23 10:50:58 +0600

    :zap: Improve Condition checking performance: Remove Manualy checking if else

--- src/pages/ChessBoard.jsx
+++ src/pages/ChessBoard.jsx
@@ -293,59 +293,118 @@ const boardStructure = [
   ],
 ];
 
+
+
+const checkCondition = (row, col) => {
+  const conditionArray = [
+    {
+      row: row - 2 >= 0 && col - 1 >= 0 ? row - 2 : null,
+      col: row - 2 >= 0 && col - 1 >= 0 ? col - 1 : null,
+    },
+    {
+      row: row - 2 >= 0 && col + 1 <= 7 ? row - 2 : null,
+      col: row - 2 >= 0 && col + 1 <= 7 ? col + 1 : null,
+    },
+    {
+      row: row - 1 >= 0 && col - 2 >= 0 ? row - 1 : null,
+      col: row - 1 >= 0 && col - 2 >= 0 ? col - 2 : null,
+    },
+    {
+      row: row - 1 >= 0 && col + 2 <= 7 ? row - 1 : null,
+      col: row - 1 >= 0 && col + 2 <= 7 ? col + 2 : null,
+    },
+    {
+      row: row + 1 <= 7 && col - 2 >= 0 ? row + 1 : null,
+      col: row + 1 <= 7 && col - 2 >= 0 ? col - 2 : null,
+    },
+    {
+      row: row + 1 <= 7 && col + 2 <= 7 ? row + 1 : null,
+      col: row + 1 <= 7 && col + 2 <= 7 ? col + 2 : null,
+    },
+    {
+      row: row + 2 <= 7 && col - 1 >= 0 ? row + 2 : null,
+      col: row + 2 <= 7 && col - 1 >= 0 ? col - 1 : null,
+    },
+    {
+      row: row + 2 <= 7 && col + 1 <= 7 ? row + 2 : null,
+      col: row + 2 <= 7 && col + 1 <= 7 ? col + 1 : null,
+    },
+  ];
+  return conditionArray;
+};
+
 const ChessBoard = () => {
   const [board, setBoard] = useState(
     JSON.parse(JSON.stringify(boardStructure))
   ); // Deep Copy
 
   // Get all possible moves for a knight
-  const getKnightMoves = (row, col) => {
+  // const getKnightMoves = (row, col) => {
+  //   // Set Board Default
+  //   setBoard(JSON.parse(JSON.stringify(boardStructure))); // Deep Copy
+  //   console.log("Row: ", row, "Col: ", col);
+  //   const moves = [];
+  //   // Up 2, left 1
+  //   if (row - 2 >= 0 && col - 1 >= 0) {
+  //     moves.push([row - 2, col - 1]);
+  //   }
+  //   // Up 2, right 1
+  //   if (row - 2 >= 0 && col + 1 < 8) {
+  //     moves.push([row - 2, col + 1]);
+  //   }
+  //   // Up 1, left 2
+  //   if (row - 1 >= 0 && col - 2 >= 0) {
+  //     moves.push([row - 1, col - 2]);
+  //   }
+  //   // Up 1, right 2
+  //   if (row - 1 >= 0 && col + 2 < 8) {
+  //     moves.push([row - 1, col + 2]);
+  //   }
+  //   // Down 2, left 1
+  //   if (row + 2 < 8 && col - 1 >= 0) {
+  //     moves.push([row + 2, col - 1]);
+  //   }
+  //   // Down 2, right 1
+  //   if (row + 2 < 8 && col + 1 < 8) {
+  //     moves.push([row + 2, col + 1]);
+  //   }
+  //   // Down 1, left 2
+  //   if (row + 1 < 8 && col - 2 >= 0) {
+  //     moves.push([row + 1, col - 2]);
+  //   }
+  //   // Down 1, right 2
+  //   if (row + 1 < 8 && col + 2 < 8) {
+  //     moves.push([row + 1, col + 2]);
+  //   }
+  //   console.log(moves);
+  //   setBoard((prevBoard) => {
+  //     const newBoard = [...prevBoard];
+  //     newBoard[row][col].canMove = true;
+  //     moves.forEach((move) => {
+  //       newBoard[move[0]][move[1]].canMove = true;
+  //     });
+  //     return newBoard;
+  //   });
+  // };
+
+  const getKnightMoves = (row, col) => { 
     // Set Board Default
     setBoard(JSON.parse(JSON.stringify(boardStructure))); // Deep Copy
     console.log("Row: ", row, "Col: ", col);
-    const moves = [];
-    // Up 2, left 1
-    if (row - 2 >= 0 && col - 1 >= 0) {
-      moves.push([row - 2, col - 1]);
-    }
-    // Up 2, right 1
-    if (row - 2 >= 0 && col + 1 < 8) {
-      moves.push([row - 2, col + 1]);
-    }
-    // Up 1, left 2
-    if (row - 1 >= 0 && col - 2 >= 0) {
-      moves.push([row - 1, col - 2]);
-    }
-    // Up 1, right 2
-    if (row - 1 >= 0 && col + 2 < 8) {
-      moves.push([row - 1, col + 2]);
-    }
-    // Down 2, left 1
-    if (row + 2 < 8 && col - 1 >= 0) {
-      moves.push([row + 2, col - 1]);
-    }
-    // Down 2, right 1
-    if (row + 2 < 8 && col + 1 < 8) {
-      moves.push([row + 2, col + 1]);
-    }
-    // Down 1, left 2
-    if (row + 1 < 8 && col - 2 >= 0) {
-      moves.push([row + 1, col - 2]);
-    }
-    // Down 1, right 2
-    if (row + 1 < 8 && col + 2 < 8) {
-      moves.push([row + 1, col + 2]);
-    }
+    const moves = checkCondition(row, col);
     console.log(moves);
     setBoard((prevBoard) => {
       const newBoard = [...prevBoard];
       newBoard[row][col].canMove = true;
       moves.forEach((move) => {
-        newBoard[move[0]][move[1]].canMove = true;
+        if (move.row !== null && move.col !== null) {
+          newBoard[move.row][move.col].canMove = true;
+        }
       });
       return newBoard;
     });
   };
+  
   return (
     <div
       style={{
